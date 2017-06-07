f2ca887c6c2e89019286d7bae9165733e55d655a
Mikhail Podtserkovskiy <mapodtserkovskiy@avito.ru>
2017-06-07 19:15:38 +0300

    fix

--- storage/migrations/mysql/20170607184935-keys.sql
+++ storage/migrations/mysql/20170607184935-keys.sql
@@ -1,9 +1,11 @@
 
 -- +migrate Up
-CREATE INDEX `part_of_name` ON `node` (`status`,`updated`);
-CREATE INDEX `part_of_name` ON `node` (`sessionId`);
-DROP INDEX `address` ON `node`;
+
+CREATE INDEX `status_of_updated` ON `node` (`status`,`updated`);
+CREATE INDEX `sessionId` ON `node` (`sessionId`);
 ALTER TABLE `node` ADD PRIMARY KEY (`address`);
-ALTER TABLE  `capabilities` ADD  CONSTRAINT `address` FOREIGN KEY (`nodeAddress`) REFERENCES `node` (`address`) ON DELETE CASCADE ON UPDATE NO ACTION;
+DROP INDEX `address` ON `node`;
+CREATE INDEX `address` ON `capabilities` (`nodeAddress`);
 
 -- +migrate Down
+SIGNAL SQLSTATE '45000' SET message_text = 'Impossible down this migration';
