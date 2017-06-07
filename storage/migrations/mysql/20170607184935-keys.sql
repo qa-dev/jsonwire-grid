241be2d699694f972780168643db94e7ea030a2a
Mikhail Podtserkovskiy <mapodtserkovskiy@avito.ru>
2017-06-07 18:54:34 +0300

    fix

--- storage/migrations/mysql/20170607184935-keys.sql
+++ storage/migrations/mysql/20170607184935-keys.sql
@@ -4,6 +4,6 @@ CREATE INDEX `part_of_name` ON `node` (`status`,`updated`);
 CREATE INDEX `part_of_name` ON `node` (`sessionId`);
 DROP INDEX `address` ON `node`;
 ALTER TABLE `node` ADD PRIMARY KEY (`address`);
-ALTER TABLE  `capabilities` ADD  CONSTRAINT `address` FOREIGN KEY (`nodeAddress`) REFERENCES `node` (`address`) ON DELETE CASCADE ON UPDATE NO ACTION
+ALTER TABLE  `capabilities` ADD  CONSTRAINT `address` FOREIGN KEY (`nodeAddress`) REFERENCES `node` (`address`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- +migrate Down
